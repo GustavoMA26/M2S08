097a8540e08beee4d334735d78e3ae500ed663d0
Gustavo <gmalmeida26@gmail.com>
2023-11-18 09:58:09 -0300

    Ex 1 - Criação do Projeto

---
+++ trackingemployee/src/main/java/devinhouse/tech/trackingemployee/TrackingemployeeApplication.java
@@ -0,0 +1,13 @@
+package devinhouse.tech.trackingemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingemployeeApplication.class, args);
+	}
+
+}
