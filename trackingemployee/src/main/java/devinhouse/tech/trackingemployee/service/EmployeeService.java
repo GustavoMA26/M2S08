f89d9fc932cc85f30ddbdc3dd4cddd633f3e437c
Gustavo <gmalmeida26@gmail.com>
2023-11-20 21:28:08 -0300

    Ex 09 - Documentação e logs

--- trackingemployee/src/main/java/devinhouse/tech/trackingemployee/service/EmployeeService.java
+++ trackingemployee/src/main/java/devinhouse/tech/trackingemployee/service/EmployeeService.java
@@ -4,18 +4,17 @@ import devinhouse.tech.trackingemployee.model.Employee;
 import devinhouse.tech.trackingemployee.model.Register;
 import devinhouse.tech.trackingemployee.model.transport.*;
 import devinhouse.tech.trackingemployee.repository.EmployeeRepository;
-import devinhouse.tech.trackingemployee.repository.RegisterRepository;
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeService.class);
 
     private final EmployeeRepository employeeRepository;
 
@@ -26,22 +25,26 @@ public class EmployeeService {
 
     @Transactional
     public EmployeeDTO create(CreateEmployeeDTO body) {
+        LOGGER.info("Starting the employee's creation");
         Employee newEmployee = new Employee(body);
         this.employeeRepository.save(newEmployee);
         return new EmployeeDTO(newEmployee);
     }
 
     public Page<EmployeeDTO> listAll(Pageable pageable) {
+        LOGGER.info("Listing all employees");
         return this.employeeRepository.findAll(pageable).map(EmployeeDTO::new);
     }
 
     public SpecifiedEmployeeDTO getSpecificEmployee(Integer id) {
+        LOGGER.info("Listing all details from a specific employee");
         return this.employeeRepository.findById(id).map(SpecifiedEmployeeDTO::new)
                 .orElseThrow(() -> new IllegalArgumentException("Employee " + id + " doesn't exist"));
     }
 
     @Transactional
     public RegisterDTO createRegister(Integer id, CreateRegisterDTO body) {
+        LOGGER.info("Creating the employee's registers");
         Employee employee = this.employeeRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Employee " + id + " doesn't exist"));
 
